c99593100c07844d659e04545dc1f2929391e88c
andreiantal <andrei.antal@kalon.ro>
2016-01-24 16:28:58 +0200

    Configure webpack with es6 and scss loaders

--- app/app.js
+++ app/app.js
@@ -1,2 +1,10 @@
-var app=angular.module('app',['ngMaterial']);
-console.log("here")
+import angular from 'angular';
+import ngMaterial from 'angular-material';
+
+require('angular-material/angular-material.scss');
+require('./app.scss');
+
+angular.module('app', [ngMaterial]);
+console.log('here1');
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,34 @@
+var path = require('path');
+
 module.exports = {
-  context : __dirname + '/app',
-  entry: './app.js',
+  context : path.join(__dirname, 'app'),
+  entry: {
+    app: './app.js'
+  },
   output: {
-    path: __dirname + '/app',
+    path: path.join(__dirname, 'app'),
     filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /(\.scss)|(\.css)$/,
+        loader: 'style!css!autoprefixer?browsers=last 2 version!sass'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: 'app',
+    noInfo: false,
+    inline: true
   }
-}
+};
